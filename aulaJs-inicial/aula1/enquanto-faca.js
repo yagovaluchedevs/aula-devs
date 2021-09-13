f60b92f1f6d16bf036adab72a50090fc2e5fe121
yagovaluchedevs <valuchedevs@gmail.com>
2021-09-13 13:37:03 -0300

    feat: ensinando a tecnica do while na pratica

---
+++ aulaJs-inicial/aula1/enquanto-faca.js
@@ -0,0 +1,25 @@
+const listUsers = [
+  {
+    name: "yago",
+    age: 20,
+  },
+  {
+    name: "cristiano",
+    age: 20,
+  },
+  {
+    name: "dona Joana",
+    age: 20,
+  },
+  {
+    name: "rene",
+    age: 20,
+  },
+];
+
+let count = 0;
+
+while (listUsers.length >= count) {
+  console.log(`quantity${count}`);
+  count++;
+}
