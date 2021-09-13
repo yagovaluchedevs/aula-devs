c1a3afc43540d35ff3290c98502678be7693c810
yagovaluchedevs <valuchedevs@gmail.com>
2021-09-13 13:21:31 -0300

    feat: entendendo o conceito de lista de objetos

---
+++ aulaJs-inicial/aula1/lista-objetos.js
@@ -0,0 +1,20 @@
+const listUsers = [
+  {
+    name: "yago",
+    age: 20,
+  },
+  {
+    name: "cristiano",
+    age: 20,
+  },
+  {
+    name: "dona Joana",
+    age: 20,
+  },
+  {
+    name: "rene",
+    age: 20,
+  },
+];
+
+console.log(listUsers);
